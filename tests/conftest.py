60a8d307e4e9aa06ca8f7a4dff80dde04afee69f
abhayshukla-gamma <abhay.shukla@gammaedge.io>
2026-02-26 22:14:58 +0530

    error resolve

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from main import app
 
 
 @pytest.fixture()  # pytets framework ke andar aata hai reusable and constint testing env deta hai they manage setup of databases and handle cleanup
-def client():
+def client():  # fixture object jisse ham test karenge fastapi routes
     return TestClient(app)  # appp ko testing env me run jisme testing perform karenge
 
 
