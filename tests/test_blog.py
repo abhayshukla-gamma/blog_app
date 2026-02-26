bd6c5853027262d15ad157a6fa4aefee25344936
abhayshukla-gamma <abhay.shukla@gammaedge.io>
2026-02-26 22:28:39 +0530

    add get test

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -6,3 +6,11 @@ def test_create_blog(client, regestered_user):
         headers={"Authorization": f"Bearer {regestered_user}"},
     )
     assert response.status_code == 200
+
+
+def test_all_blogs(client, regestered_user):
+    response = client.get(
+        "/blog/getblogs", headers={"Authorization": f"Bearer {regestered_user}"}
+    )
+
+    assert response.status_code == 200
